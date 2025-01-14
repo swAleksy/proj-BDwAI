0a4bb9e1e76935001ed1afae7bf5e3b18783c8d0
swAleksy <94024221+swAleksy@users.noreply.github.com>
2025-01-14 15:08:02 +0100

    tc1

--- projBDwAI/Models/Priority.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace projBDwAI.Models
-{
-    public class Priority
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public List<Bug> Bugs { get; set; }
-    }
-}
--- projBDwAI/Program.cs
+++ projBDwAI/Program.cs
@@ -1,7 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using projBDwAI.Models.Context;
-using System;
-
 namespace projBDwAI
 {
     public class Program
@@ -12,8 +8,6 @@ namespace projBDwAI
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
